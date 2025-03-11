3b30e5e23eab69b6d5fa7e3a43e1a0f433151e06
John Scholefield <johnscho579@gmail.com>
2025-03-11 10:51:12 +0000

    add the GET /API/articles endpoint

--- model.js
+++ model.js
@@ -17,5 +17,11 @@ function fetchArticleById(article_id){
     })
 }
 
+function fetchAllArticles(){
+    return db.query("SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC").then(({rows}) => { 
+        return rows
+        })
+}
 
-module.exports = {fetchAllTopics, fetchArticleById}
+
+module.exports = {fetchAllTopics, fetchArticleById, fetchAllArticles}
