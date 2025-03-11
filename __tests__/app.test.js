d17e04206e846b5ad4eebf8b57d0d62d939ea3c1
John Scholefield <johnscho579@gmail.com>
2025-03-11 10:47:34 +0000

    add the GET /API/articles endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,6 @@ const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
 const request = require("supertest");
 const app = require("../app");
-const {convertTimestampToDate} = require("../db/seeds/utils")
 
 beforeEach(() => {
   return seed(data);
@@ -57,7 +56,6 @@ describe("GET /api/articles/:article_id", () => {
       .get("/api/articles/4")
       .expect(200)
       .then(({ body }) => {
-        console.log(body.article)
         expect(body.article.author).toBe("rogersop")
         expect(body.article.title).toBe("Student SUES Mitch!")
         expect(body.article.article_id).toBe(4)
@@ -87,3 +85,37 @@ describe("GET /api/articles/:article_id", () => {
   })
 })
 })
+
+
+describe("GET /api/articles", () => {
+  test("200: Responds with an array of all the articles", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles).toHaveLength(13)
+        body.articles.forEach(article => {
+          expect(typeof article.author).toBe('string')
+          expect(typeof article.title).toBe('string')
+          expect(typeof article.article_id).toBe('number')
+          expect(typeof article.topic).toBe('string')
+          expect(typeof article.created_at).toBe('string')
+          expect(typeof article.votes).toBe('number')
+          expect(typeof article.body).toBe("undefined")
+          expect(typeof article.article_img_url).toBe('string')
+          expect(typeof article.comment_count).toBe('string')
+        })
+      })
+  })
+
+  test("The articles in the array are sorted by date in descending order", () => {
+    return request(app)
+      .get("/api/articles")
+      .then(({ body }) => {
+        expect(body.articles[0].created_at).toBe("2020-11-03T09:12:00.000Z")
+        expect(body.articles[12].created_at).toBe("2020-01-07T14:08:00.000Z")
+      })
+  })
+})
+
+
